0832c2aa87f47a3b1162d577812e576260361902
vipulbeniwal2004 <vipulbeniwal2004@gmail.com>
2024-09-07 16:03:43 +0530

    code

---
+++ main.js
@@ -0,0 +1,11 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const  genai = new GoogleGenerativeAI("AIzaSyBnut3BNZYCY6cfIKyf33jJYyi7yySztwE");
+
+const model = genai.getGenerativeModel({
+  model : "gemini-1.5-pro"
+});
+
+const r = await model.generateContent("What is node js?");
+
+console.log(r.response.text());
