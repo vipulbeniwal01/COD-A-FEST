ae8f63bd7ca249dbcf0614552866935c90c27768
vipulbeniwal2004 <vipulbeniwal2004@gmail.com>
2024-09-07 16:36:02 +0530

    code

--- main.js
+++ main.js
@@ -8,11 +8,4 @@ const model = genai.getGenerativeModel({
 
 const r = await model.generateContent("What is node js?");
 
-// console.log(r.response.text());
-// <<<<<<< main
-
-// ;wheowvbewofheowihvawoi
-
-// // "can you see this change"
-// =======
-// >>>>>>> main
+console.log(r.response.text());
